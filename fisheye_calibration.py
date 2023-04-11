4d16a3bf4ab1fc86cd58d69749706ca6c85e47c3
jimmy <jimmy.yang@moprobo.com>
2023-04-11 10:59:22 +0800

    first version
    Signed-off-by: jimmy <jimmy.yang@moprobo.com>

---
+++ fisheye_calibration.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+import glob
+ 
+ 
+def get_K_and_D(checkerboard, imgsPath):
+ 
+    CHECKERBOARD = checkerboard
+    subpix_criteria = (cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 0.1)
+    calibration_flags = cv2.fisheye.CALIB_RECOMPUTE_EXTRINSIC+cv2.fisheye.CALIB_CHECK_COND+cv2.fisheye.CALIB_FIX_SKEW
+    objp = np.zeros((1, CHECKERBOARD[0]*CHECKERBOARD[1], 3), np.float32)
+    objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
+    _img_shape = None
+    objpoints = []
+    imgpoints = []
+    print(imgsPath)
+    images = glob.glob(imgsPath + '/*.jpg')
+    print(images)
+    for fname in images:
+        print(fname)
+        img = cv2.imread(fname)
+        if _img_shape == None:
+            _img_shape = img.shape[:2]
+        else:
+            assert _img_shape == img.shape[:2], "All images must share the same size."
+ 
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD,cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+        if ret == True:
+            objpoints.append(objp)
+            cv2.cornerSubPix(gray,corners,(3,3),(-1,-1),subpix_criteria)
+            imgpoints.append(corners)
+    N_OK = len(objpoints)
+    K = np.zeros((3, 3))
+    D = np.zeros((4, 1))
+    rvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
+    tvecs = [np.zeros((1, 1, 3), dtype=np.float64) for i in range(N_OK)]
+    rms, _, _, _, _ = cv2.fisheye.calibrate(objpoints,imgpoints,gray.shape[::-1],K,D,rvecs,tvecs,calibration_flags,(cv2.TERM_CRITERIA_EPS+cv2.TERM_CRITERIA_MAX_ITER, 30, 1e-6))
+    DIM = _img_shape[::-1]
+    print("Found " + str(N_OK) + " valid images for calibration")
+    print("DIM=" + str(_img_shape[::-1]))
+    print("K=np.array(" + str(K.tolist()) + ")")
+    print("D=np.array(" + str(D.tolist()) + ")")
+    return DIM, K, D
+ 
+ 
+def undistort(img_path,K,D,DIM,scale=0.6,imshow=False):
+    img = cv2.imread(img_path)
+    dim1 = img.shape[:2][::-1]  #dim1 is the dimension of input image to un-distort
+    assert dim1[0]/dim1[1] == DIM[0]/DIM[1], "Image to undistort needs to have same aspect ratio as the ones used in calibration"
+    if dim1[0]!=DIM[0]:
+        img = cv2.resize(img,DIM,interpolation=cv2.INTER_AREA)
+    Knew = K.copy()
+    if scale:#change fov
+        Knew[(0,1), (0,1)] = scale * Knew[(0,1), (0,1)]
+    map1, map2 = cv2.fisheye.initUndistortRectifyMap(K, D, np.eye(3), Knew, DIM, cv2.CV_16SC2)
+    undistorted_img = cv2.remap(img, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+    if imshow:
+        cv2.imshow("undistorted", undistorted_img)
+    return undistorted_img
+ 
+if __name__ == '__main__':
+ 
+   #(1)开始使用图片来获取内参和畸变系数
+    DIM, K, D = get_K_and_D((8,11), 'picture')
+ 
+   #(2)利用得到内参和畸变系数畸变矫正图片进行测试
+    # DIM=(1600, 1200)
+    # K=np.array([[380.30264530555115, 0.0, 835.7633233422137], [0.0, 380.35608246363023, 648.7089226599809], [0.0, 0.0, 1.0]])
+    # D=np.array([[-0.03261537438126976], [0.011064666775933927], [-0.02408476631916144], [0.014821224544658729]])
+    # img = undistort('./picture/1.jpg',K,D,DIM)
+    # cv2.imwrite('./picture/pig_checkerboard.jpg', img)
+    
